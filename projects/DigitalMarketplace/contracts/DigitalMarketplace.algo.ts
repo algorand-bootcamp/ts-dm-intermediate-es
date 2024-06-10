d7c9eb6f8af4f96e1a9226f8ea799ada7824c056
Evert Diaz <evertdiazb@gmail.com>
2024-06-10 18:35:41 -0500

    session 1

--- projects/DigitalMarketplace/contracts/DigitalMarketplace.algo.ts
+++ projects/DigitalMarketplace/contracts/DigitalMarketplace.algo.ts
@@ -1,57 +1,128 @@
 import { Contract } from '@algorandfoundation/tealscript';
 
+const forSaleMBR = 2_500 + 400 * 56;
+
 export class DigitalMarketplace extends Contract {
-  /**
-   * Calculates the sum of two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The sum of a and b
-   */
-  private getSum(a: uint64, b: uint64): uint64 {
-    return a + b;
+  forSaleBoard = BoxMap<{ owner: Address; asa: uint64 }, { deposited: uint64; unitaryPrice: uint64 }>();
+
+  // Permitir que el contrato reciba assets
+  allowAsset(mbrPay: PayTxn, asset: AssetID) {
+    assert(!this.app.address.isOptedInToAsset(asset));
+
+    verifyTxn(mbrPay, {
+      receiver: this.app.address,
+      amount: globals.assetOptInMinBalance,
+    });
+
+    sendAssetTransfer({
+      xferAsset: asset,
+      assetAmount: 0,
+      assetReceiver: this.app.address,
+    });
   }
 
-  /**
-   * Calculates the difference between two numbers
-   *
-   * @param a
-   * @param b
-   * @returns The difference between a and b.
-   */
-  private getDifference(a: uint64, b: uint64): uint64 {
-    return a >= b ? a - b : b - a;
+  // Metodo para realizar un deposito de assets
+
+  // Metodo de primer deposito
+
+  // Global State -> Almacenamiento relacionado al contrato
+  // Local State -> Almacenamiento relacion al usuario
+  // Boxes -> Almacenamientos en Algorand ilimtado max 32kb x box, Boxes sin limite por contrato
+
+  // Boxes : Key->Value ----- Key: {owner,assetID}  //  Value: {deposited,unitaryPrice}
+  firstDeposit(mbrPay: PayTxn, xfer: AssetTransferTxn, unitaryPrice: uint64) {
+    assert(!this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).exists);
+
+    // MBR del box es 2_500 + 400 * byte
+    // Calculo de bytes = NumBytes(address) + NumBytes(uint64) + NumBytes(uint64) + NumBytes(uint64)
+    //  Bytes = 32 + 8 + 8 + 8 = 56 Byte
+    // MBR = 2_500 + 400 * 56
+    verifyPayTxn(mbrPay, {
+      sender: this.txn.sender,
+      receiver: this.app.address,
+      amount: forSaleMBR,
+    });
+
+    verifyAssetTransferTxn(xfer, {
+      sender: this.txn.sender,
+      assetReceiver: this.app.address,
+      assetAmount: { greaterThan: 0 },
+    });
+
+    this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).value = {
+      deposited: xfer.assetAmount,
+      unitaryPrice: unitaryPrice,
+    };
   }
 
-  /**
-   * A method that takes two numbers and does either addition or subtraction
-   *
-   * @param a The first uint64
-   * @param b The second uint64
-   * @param operation The operation to perform. Can be either 'sum' or 'difference'
-   *
-   * @returns The result of the operation
-   */
-  doMath(a: uint64, b: uint64, operation: string): uint64 {
-    let result: uint64;
+  // Metodo de depositos posteriores
+  deposit(xfer: AssetTransferTxn) {
+    assert(this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).exists);
 
-    if (operation === 'sum') {
-      result = this.getSum(a, b);
-    } else if (operation === 'difference') {
-      result = this.getDifference(a, b);
-    } else throw Error('Invalid operation');
+    verifyAssetTransferTxn(xfer, {
+      sender: this.txn.sender,
+      assetReceiver: this.app.address,
+      assetAmount: { greaterThan: 0 },
+    });
 
-    return result;
+    const currentBalance = this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).value.deposited;
+    const currentPrice = this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).value.unitaryPrice;
+
+    this.forSaleBoard({ owner: this.txn.sender, asa: xfer.xferAsset.id }).value = {
+      deposited: currentBalance + xfer.assetAmount,
+      unitaryPrice: currentPrice,
+    };
   }
 
-  /**
-   * A demonstration method used in the AlgoKit fullstack template.
-   * Greets the user by name.
-   *
-   * @param name The name of the user to greet.
-   * @returns A greeting message to the user.
-   */
-  hello(name: string): string {
-    return 'Hello, ' + name;
+  // Metodo para modificar el precio de los assets a venderse
+  setPrice(asset: uint64, newPrice: uint64) {
+    assert(this.forSaleBoard({ owner: this.txn.sender, asa: asset }).exists);
+
+    const currentDeposit = this.forSaleBoard({ owner: this.txn.sender, asa: asset }).value.deposited;
+
+    this.forSaleBoard({ owner: this.txn.sender, asa: asset }).value = {
+      deposited: currentDeposit,
+      unitaryPrice: newPrice,
+    };
+  }
+
+  // Metodo para hacer la compra de los assets por parte del usuario
+  buy(owner: Address, asset: AssetID, buyPay: PayTxn, quantity: uint64) {
+    const currentPrice = this.forSaleBoard({ owner: owner, asa: asset.id }).value.unitaryPrice;
+    const currentDeposit = this.forSaleBoard({ owner: owner, asa: asset.id }).value.deposited;
+    const amountToBePaid = currentPrice * quantity;
+
+    verifyPayTxn(buyPay, {
+      sender: this.txn.sender,
+      receiver: owner,
+      amount: amountToBePaid,
+    });
+
+    sendAssetTransfer({
+      xferAsset: asset,
+      assetReceiver: this.txn.sender,
+      assetAmount: quantity,
+    });
+
+    this.forSaleBoard({ owner: owner, asa: asset.id }).value = {
+      deposited: currentDeposit - quantity,
+      unitaryPrice: currentPrice,
+    };
+  }
+
+  // Metodo para que el comprador retire sus ganancias y assets restantes
+  withdraw(asset: AssetID) {
+    const currentDeposit = this.forSaleBoard({ owner: this.txn.sender, asa: asset.id }).value.deposited;
+
+    sendAssetTransfer({
+      xferAsset: asset,
+      assetReceiver: this.txn.sender,
+      assetAmount: currentDeposit,
+    });
+
+    sendPayment({
+      receiver: this.txn.sender,
+      amount: forSaleMBR,
+    });
   }
 }
